70131f77c55c675b4fd66b5b2e446e4ef184e017
pasindusampath <pasindub32@gmail.com>
2025-11-29 20:31:20 +0530

    webpack changes

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,4 +1,5 @@
 import type { NextConfig } from 'next';
+import path from 'path';
 
 const nextI18nextConfig = require('./next-i18next.config.js');
 
@@ -10,6 +11,16 @@ const nextConfig: NextConfig = {
     externalDir: true,
   },
   i18n: nextI18nextConfig.i18n,
+  webpack: (config, { isServer }) => {
+    // Resolve shared library paths for webpack
+    // Point to libs/shared so that @nx-mono-repo-deployment-test/shared/src/... resolves correctly
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@nx-mono-repo-deployment-test/shared': path.resolve(__dirname, '../../libs/shared'),
+    };
+    
+    return config;
+  },
 };
 
 export default nextConfig;
